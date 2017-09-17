70e00c3c8d0465e2b49f8299bf4bbf2eec0fb0b4
Aaron Heath <aaron@aaronheath.com>
2017-09-18 06:26:09 +0930

    Was out by one day

--- src/app/countdown/countdown.component.ts
+++ src/app/countdown/countdown.component.ts
@@ -22,6 +22,7 @@ export class CountdownComponent implements OnInit {
   countdown;
   daysLeft;
   wrapperClasses;
+  when;
 
   countdowns: Countdowns = {
     australia: {when: [2018, 11, 11], msg: 'Australia'},
@@ -38,13 +39,14 @@ export class CountdownComponent implements OnInit {
       this.refreshNow();
       this.refreshCurrentTime();
 
-      this.daysLeft = this.daysUntil(new Date(this.countdown.when[0], this.countdown.when[1], this.countdown.when[2]));
+      this.daysLeft = this.daysUntil(this.when);
     }, 1000);
 
     this.route.params.subscribe((data) => {
       this.wrapperClasses = `countdown ${data.countdown}`;
       this.countdown = this.countdowns[data.countdown];
-      this.daysLeft = this.daysUntil(new Date(this.countdown.when[0], this.countdown.when[1], this.countdown.when[2]));
+      this.setWhen();
+      this.daysLeft = this.daysUntil(this.when);
     });
   }
 
@@ -64,5 +66,16 @@ export class CountdownComponent implements OnInit {
     }
 
     return Math.ceil((when.getTime() - this.now.getTime()) / (1000 * 3600 * 24));
+  }
+
+  setWhen() {
+    const year = this.countdown.when[0] ? this.countdown.when[0] : undefined;
+    const month = this.countdown.when[1] ? this.countdown.when[1] : undefined;
+    const day = this.countdown.when[2] ? this.countdown.when[2] : undefined;
+    const hour = this.countdown.when[3] ? this.countdown.when[3] : 23;
+    const min = this.countdown.when[4] ? this.countdown.when[4] : 59;
+    const sec = this.countdown.when[5] ? this.countdown.when[5] : 59;
+
+    this.when = new Date(year, month, day, hour, min, sec);
   }
 }
