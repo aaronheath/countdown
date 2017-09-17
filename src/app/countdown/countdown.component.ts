29da2bd160f2aed072a0f3c945f7ca53d8eb9dd3
Aaron Heath <aaron@aaronheath.com>
2017-09-18 06:49:29 +0930

    Was out by a month

--- src/app/countdown/countdown.component.ts
+++ src/app/countdown/countdown.component.ts
@@ -26,8 +26,8 @@ export class CountdownComponent implements OnInit {
   when;
 
   countdowns: Countdowns = {
-    australia: {when: [2018, 11, 11], msg: 'Australia'},
-    la: {when: [2018, 7, 22], msg: 'Los Angeles'},
+    australia: {when: [2018, 10, 11], msg: 'Australia'},
+    la: {when: [2018, 6, 22], msg: 'Los Angeles'},
   };
 
   constructor(private route: ActivatedRoute) {}
