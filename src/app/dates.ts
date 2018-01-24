959bba376d9f0848d3a74b23e68cdbd30b1a12d5
Aaron Heath <aaron@aaronheath.com>
2018-01-24 13:39:44 +1030

    Updated images, added laracon au.

--- src/app/dates.ts
+++ src/app/dates.ts
@@ -16,5 +16,6 @@ export const dates: Countdowns = {
   // r15: {when: [2018, 6, 1], msg: 'Round 15 v NM'},
   hoh: {when: [2018, 6, 12], msg: 'House of Hardcore'},
   la: {when: [2018, 6, 22], msg: 'Los Angeles'},
+  laraconau: {when: [2018, 9, 17], msg: 'Laracon AU'},
   australia: {when: [2018, 10, 11], msg: 'Australia'},
 };
