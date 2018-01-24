924950b50ba0ff42111950d40bdd0b1718542832
Aaron Heath <aaron@aaronheath.com>
2018-01-24 13:27:42 +1030

    Updated events

--- src/app/dates.ts
+++ src/app/dates.ts
@@ -8,12 +8,13 @@ export interface Countdowns {
 }
 
 export const dates: Countdowns = {
-  wk: {when: [2018, 0, 4], msg: 'Wrestle Kingdom'},
   perth: {when: [2018, 1, 10], msg: 'UFC Perth'},
+  njpwadl: {when: [2018, 1, 16], msg: 'NJPW Adelaide'},
+  njpwmel: {when: [2018, 1, 17], msg: 'NJPW Melbourne'},
   wsw: {when: [2018, 2, 18], msg: 'World Series Wrestling'},
-  r1: {when: [2018, 2, 23], msg: 'Round 1 v ADL'},
   anzac: {when: [2018, 3, 25], msg: 'ANZAC Day'},
-  r15: {when: [2018, 6, 1], msg: 'Round 15 v NM'},
+  // r15: {when: [2018, 6, 1], msg: 'Round 15 v NM'},
+  hoh: {when: [2018, 6, 12], msg: 'House of Hardcore'},
   la: {when: [2018, 6, 22], msg: 'Los Angeles'},
   australia: {when: [2018, 10, 11], msg: 'Australia'},
 };
