32004dcc655ddc6acefcc2b9224ed7d3bb01383d
Aaron Heath <aaron@aaronheath.com>
2018-03-10 15:33:49 +1030

    Added WSW-June, AusGP. Removed past.

--- src/app/dates.ts
+++ src/app/dates.ts
@@ -8,12 +8,11 @@ export interface Countdowns {
 }
 
 export const dates: Countdowns = {
-  perth: {when: [2018, 1, 10], msg: 'UFC Perth'},
-  njpwadl: {when: [2018, 1, 16], msg: 'NJPW Adelaide'},
-  njpwmel: {when: [2018, 1, 17], msg: 'NJPW Melbourne'},
   wsw: {when: [2018, 2, 18], msg: 'World Series Wrestling'},
+  f1: {when: [2018, 2, 25], msg: 'Australian GP'},
   anzac: {when: [2018, 3, 25], msg: 'ANZAC Day'},
   // r15: {when: [2018, 6, 1], msg: 'Round 15 v NM'},
+  'wsw-june': {when: [2018, 5, 24], msg: 'World Series Wrestling'},
   hoh: {when: [2018, 6, 12], msg: 'House of Hardcore'},
   la: {when: [2018, 6, 22], msg: 'Los Angeles'},
   laraconau: {when: [2018, 9, 17], msg: 'Laracon AU'},
