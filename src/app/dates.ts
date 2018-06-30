c6ce2efc44ef8266dbde462a01e02a40bec13907
Aaron Heath <aaron@aaronheath.com>
2018-06-30 17:15:37 +0930

    Removed ESSvNM, HoH. Added UKIP

--- src/app/dates.ts
+++ src/app/dates.ts
@@ -8,10 +8,8 @@ export interface Countdowns {
 }
 
 export const dates: Countdowns = {
-  'wsw-june': {when: [2018, 5, 24], msg: 'World Series Wrestling'},
-  'r15': {when: [2018, 5, 30], msg: 'Round 15 v NM'},
-  'hoh': {when: [2018, 6, 12], msg: 'House of Hardcore'},
   'la': {when: [2018, 6, 22], msg: 'Los Angeles'},
+  'ukip': {when: [2018, 8, 3], msg: 'Nigel Farage'},
   'wwe': {when: [2018, 9, 6], msg: 'WWE Melbourne'},
   'laraconau': {when: [2018, 9, 17], msg: 'Laracon AU'},
   'australia': {when: [2018, 10, 11], msg: 'Australia'},
