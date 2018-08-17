acbef666df8225573384964b0a480bcfd0a899f6
Aaron Heath <aaron@aaronheath.com>
2018-08-18 00:22:24 +0930

    Added WSW Melb, ANZAC Day 2019 break, Book of Mormon. Removed LA.

--- src/app/dates.ts
+++ src/app/dates.ts
@@ -8,9 +8,12 @@ export interface Countdowns {
 }
 
 export const dates: Countdowns = {
-  'la': {when: [2018, 6, 22], msg: 'Los Angeles'},
+  // 'la': {when: [2018, 6, 22], msg: 'Los Angeles'},
   'ukip': {when: [2018, 8, 3], msg: 'Nigel Farage'},
   'wwe': {when: [2018, 9, 6], msg: 'WWE Melbourne'},
   'laraconau': {when: [2018, 9, 17], msg: 'Laracon AU'},
   'australia': {when: [2018, 10, 11], msg: 'Australia'},
+  'wsw-melbourne': {when: [2018, 10, 23], msg: 'WSW Melbourne'},
+  'anzac': {when: [2019, 3, 19], msg: 'ANZAC Day'},
+  'mormon': {when: [2019, 6, 13], msg: 'Book of Mormon'},
 };
