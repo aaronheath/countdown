ec05cd9b5a72b74dc08dd332819d63acb8d12355
Aaron Heath <aaron@aaronheath.com>
2018-09-30 09:23:26 +0930

    Added Germantown Road Settlement

--- src/app/dates.ts
+++ src/app/dates.ts
@@ -12,6 +12,7 @@ export const dates: Countdowns = {
   // 'ukip': {when: [2018, 8, 3], msg: 'Nigel Farage'},
   'wwe': {when: [2018, 9, 6], msg: 'WWE Melbourne'},
   'laraconau': {when: [2018, 9, 17], msg: 'Laracon AU'},
+  'settlement': {when: [2018, 10, 2], msg: 'Settlement'},
   'australia': {when: [2018, 10, 11], msg: 'Australia'},
   'wsw-melbourne': {when: [2018, 10, 23], msg: 'WSW Melbourne'},
   'anzac': {when: [2019, 3, 19], msg: 'ANZAC Day'},
