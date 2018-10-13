240513dec83b5ac829c2b26cc91fe7fe334187e1
Aaron Heath <aaron@aaronheath.com>
2018-10-13 11:47:51 +1030

    Added UFC Adelaide

--- src/app/dates.ts
+++ src/app/dates.ts
@@ -15,6 +15,7 @@ export const dates: Countdowns = {
   'settlement': {when: [2018, 10, 2], msg: 'Settlement'},
   'australia': {when: [2018, 10, 11], msg: 'Australia'},
   'wsw-melbourne': {when: [2018, 10, 23], msg: 'WSW Melbourne'},
+  'ufc-adelaide': {when: [2018, 11, 2], msg: 'UFC'},
   'anzac': {when: [2019, 3, 19], msg: 'ANZAC Day'},
   'mormon': {when: [2019, 6, 13], msg: 'Book of Mormon'},
 };
