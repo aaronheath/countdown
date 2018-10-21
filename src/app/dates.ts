5586f7e6a9fbfc3f992f3b1045e1beab59f9e519
Aaron Heath <aaron@aaronheath.com>
2018-10-21 11:32:06 +1030

    Removed Laracon AU

--- src/app/dates.ts
+++ src/app/dates.ts
@@ -8,10 +8,6 @@ export interface Countdowns {
 }
 
 export const dates: Countdowns = {
-  // 'la': {when: [2018, 6, 22], msg: 'Los Angeles'},
-  // 'ukip': {when: [2018, 8, 3], msg: 'Nigel Farage'},
-  // 'wwe': {when: [2018, 9, 6], msg: 'WWE Melbourne'},
-  'laraconau': {when: [2018, 9, 17], msg: 'Laracon AU'},
   'settlement': {when: [2018, 10, 2], msg: 'Settlement'},
   'australia': {when: [2018, 10, 11], msg: 'Australia'},
   'wsw-melbourne': {when: [2018, 10, 23], msg: 'WSW Melbourne'},
