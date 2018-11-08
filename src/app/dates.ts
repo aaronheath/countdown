f2e33e157e2a49a34086a6a311309c38eca9bb78
Aaron Heath <aaron.heath@staff.nuksope.com.au>
2018-11-09 07:43:39 +1030

    Removed Settlement. Added UFC 234, Rnd 2, Rnd 4.

--- src/app/dates.ts
+++ src/app/dates.ts
@@ -8,10 +8,12 @@ export interface Countdowns {
 }
 
 export const dates: Countdowns = {
-  'settlement': {when: [2018, 10, 2], msg: 'Settlement'},
   'australia': {when: [2018, 10, 11], msg: 'Australia'},
   'wsw-melbourne': {when: [2018, 10, 23], msg: 'WSW Melbourne'},
-  'ufc-adelaide': {when: [2018, 11, 2], msg: 'UFC'},
+  'ufc-adelaide': {when: [2018, 11, 2], msg: 'UFC ADL'},
+  'ufc-234': {when: [2019, 1, 10], msg: 'UFC 234'},
+  'r2': {when: [2019, 2, 30], msg: 'Rnd 2 v STK (Marvel)'},
+  'r4': {when: [2019, 3, 13], msg: 'Rnd 4 v BL (MCG)'},
   'anzac': {when: [2019, 3, 19], msg: 'ANZAC Day'},
   'mormon': {when: [2019, 6, 13], msg: 'Book of Mormon'},
 };
